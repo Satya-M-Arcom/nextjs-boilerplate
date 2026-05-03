e94b1696cac230e3a39550d9a3d6e5304f36c202
Satya-M-Arcom <satyamarcom@gmail.com>
2026-05-03 06:25:09 +0530

    Initial quantophobiadeleted setup

---
+++ next.config.mjs
@@ -0,0 +1,17 @@
+import remarkMath from 'remark-math'
+import rehypeKatex from 'rehype-katex'
+import createMDX from '@next/mdx'
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+}
+
+const withMDX = createMDX({
+  options: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
+  },
+})
+
+export default withMDX(nextConfig)
